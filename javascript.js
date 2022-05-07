a29bbd33241e5f848d9e851e327a216cfb92dd35
Oscar <oscarportillo721@gmail.com>
2022-05-06 21:44:16 -0300

    Added event handlers to capture mousemove events. When mouse is pressed down, it should start drawing on the squares, and when mouse button is released, the user will no longer be able to draw

--- javascript.js
+++ javascript.js
@@ -5,13 +5,44 @@ function createGrid() {
 
     //create a for loop that creates a "16x16" (256) divs
 
-    for (let i = 0; i < 257; i++) {
+    for (let i = 0; i < 500; i++) {
 
         container.appendChild(newDiv.cloneNode(true));
-        newDiv.classList.add('grid');
-
+        newDiv.classList.add('boxes');
     }
 
 }
 
 createGrid();
+
+
+let divs = document.querySelectorAll('.boxes');
+console.log(divs);
+
+
+let isDrawing = false;
+
+
+divs.forEach(smallDivs => {
+    smallDivs.addEventListener('mousedown', () => {
+        isDrawing = true;
+        console.log(isDrawing);
+    })
+
+    smallDivs.addEventListener('mouseup', () => {
+        isDrawing = false;
+        console.log(isDrawing);
+    })
+});
+
+divs.forEach(smallDivs => {
+    smallDivs.addEventListener('mouseover', () => {
+
+        if(isDrawing === true) {
+            smallDivs.classList.add('hover');
+        }
+
+    })
+})
+
+
